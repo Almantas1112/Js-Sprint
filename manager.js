42a58ce2c623bcaae148c8c6674419e7ea66da16
Almantas1112 <aalmantass1112@gmail.com>
2022-04-22 15:31:42 +0300

    Adding update button

--- manager.js
+++ manager.js
@@ -12,13 +12,20 @@ function submit() {
         newBtn.className = 'buttons';
         newBtn.id = 'button';
         newDiv.appendChild(newBtn);
-        //Creating button
+        //Creating remove button
         const button = document.createElement("button");
         button.type = 'button';
         button.id = 'delete';
         button.setAttribute("onclick","remove()");
         button.textContent = 'X';
         newBtn.appendChild(button);
+        //Creating update button
+        const buttonUpdate = document.createElement("button");
+        buttonUpdate.type = 'button';
+        buttonUpdate.id = 'update';
+        buttonUpdate.setAttribute("onclick","update()");
+        buttonUpdate.textContent = '+';
+        newBtn.appendChild(buttonUpdate);
     }
     else {
         alert("Nothing added to list!")
@@ -26,7 +33,41 @@ function submit() {
 }
 
 function remove() {
-    var evnt = event.target;
-    var parent = evnt.parentElement.parentElement;
+    let evnt = event.target;
+    let parent = evnt.parentElement.parentElement;
     parent.remove(parent);
+}
+
+function update() {
+    newItem = prompt("Please update item!");
+    if(newItem != null)
+    {
+        let evnt = event.target;
+        let parent = evnt.parentElement.parentElement;
+        console.log(parent);
+        console.log(parent.textContent = newItem);
+        //Creating button div
+        const newBtn = document.createElement("div");
+        newBtn.className = 'buttons';
+        newBtn.id = 'button';
+        parent.appendChild(newBtn);
+        //Creating remove button
+        const button = document.createElement("button");
+        button.type = 'button';
+        button.id = 'delete';
+        button.setAttribute("onclick","remove()");
+        button.textContent = 'X';
+        newBtn.appendChild(button);
+        //Creating update button
+        const buttonUpdate = document.createElement("button");
+        buttonUpdate.type = 'button';
+        buttonUpdate.id = 'update';
+        buttonUpdate.setAttribute("onclick","update()");
+        buttonUpdate.textContent = '+';
+        newBtn.appendChild(buttonUpdate);
+    }
+    else
+    {
+        prompt("Nothing updated! please enter new item or cancel!");
+    }
 }
